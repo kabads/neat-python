42273c864e5165570f516235a175d8887fcc283b
adam <adam@monkeez.org>
2020-05-31 11:29:31 +0100

    Added config-feedforward.txt

--- flappy_bird_tutorial.py
+++ flappy_bird_tutorial.py
@@ -200,3 +200,15 @@ def main():
     pygame.quit()
     quit()
 main()
+
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_path)
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
